cc4b54dc880d85a8746d55f3cfb1bca4fef70434
infopkrajput <pknatic@gmail.com>
2024-10-01 15:48:11 +0530

    01-10-2024

---
+++ LetUsJAVA/chapter6/ch6Ca.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+@SuppressWarnings({ "resource", "unused" })
+public class ch6Ca {
+    public static void main(String[] args) {
+        boolean exit = true;
+        while (exit) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("1. Factorial of a number : ");
+            System.out.println("2. Prime or not : ");
+            System.out.println("3. Odd or Even : ");
+            System.out.println("4. Exit ");
+            System.out.print("Enter your choice : ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter any number to calculate factorial : ");
+                    int x = sc.nextInt();
+                    int fact = 1;
+                    for (int i = 1; i <= x; i++) {
+                        fact *= i;
+                    }
+                    System.out.println("Factorial of " + x + " is " + fact);
+                    break;
+                case 2:
+                    System.out.print("Enter any number to check if it is prime : ");
+                    int num = sc.nextInt();
+                    boolean isPrime = true;
+                    for (int i = 2; i < num; i++) {
+                        if (num % i == 0) {
+                            System.out.println(num + " is not a prime number.");
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime) {
+                        System.out.println(num + " is a prime number.");
+                    }
+                    break;
+                case 3:
+                    System.out.print("Enter any number to check if it is odd or even : ");
+                    int n = sc.nextInt();
+                    if (n % 2 == 0) {
+                        System.out.println(n + " is an even number.");
+                    } else {
+                        System.out.println(n + " is an odd number.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    exit = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
+            }
+        }
+    }
+
+}
