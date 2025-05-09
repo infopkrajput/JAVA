05e705e07268fdf19e1122e017b7397e6a21687b
Pushpendra Kumar <p.k.rajput@hotmail.com>
2025-05-09 22:17:15 +0530

    update

--- Projects/CompleteJAVA/src/com/pk/completejava/controlflow/WhilLoop.java
+++ Projects/CompleteJAVA/src/com/pk/completejava/controlflow/WhilLoop.java
@@ -2,10 +2,10 @@ package com.pk.completejava.controlflow;
 
 public class WhilLoop {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 5) {
+        int i = 15000;
+        while (i > 0) {
             System.out.println("Hello World " + i);
-            i++;
+            i /= 3;
         }
     }
 }
